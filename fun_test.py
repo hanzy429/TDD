04577f875f1e0272c9054e5db8fd30fa7c286c39
hanzy429 <hanzy429@outlook.com>
2020-04-07 10:58:18 +0800

    Refactor home page view to use template

--- fun_test.py
+++ fun_test.py
@@ -10,30 +10,26 @@ class NewVisitorTest(unittest.TestCase): #(1)
     def tearDown(self):
         self.browser.quit()
         
-    def test_can_start_a_list_and_retrieve_it_later(self):#2
-        # Edith has heard about a cool new online to-do app. she goes
-        #to check out its homepage
-        self.browser.get('http://localhost:8000')   #(3)
-        self.assertIn('To-Do',self.browser.title)#(4)
-        header_text = self.browser.find_element_by_tag_name('h1').text
-        self.assertIn('To-Do',header_text)
+    def test_can_start_a_list_and_retrieve_it_later(self):
+          self.browser.get('http://localhost:8000')
 
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertEqual(
-            inputbox.get_attribute('placeholder'),
-            'Enter a to-do item'
-            )
-
-        inputbox.send_keys('Buy peacok feathers')
-        inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-
-        table = self.browser.find_element_by_id('id_lisy_table')        
-        rows = table.find_element_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacok feathers' for row in rows)
-            )
-        self.fail('Finish the test!')
+          self.assertIn('To-Do',self.browser.title)
+          header_text = self.browser.find_element_by_tag_name('h1').text
+          self.assertIn('To-Do',header_text)
+          inputbox = self.browser.find_element_by_id('id_new_item')
+          self.assertEqual(
+              inputbox.get_attribute('placeholder'),
+              'Enter a to-do item'
+              )
+          inputbox.send_keys('Buy peacock feathers')
+          inputbox.send_keys(Keys.ENTER)
+          time.sleep(1)
+          table = self.browser.find_element_by_id('id_list_table')
+          rows = table.find_elements_by_tag_name('tr')
+          self.assertTrue(
+              any(row.text == '1:Buy peacock feathers' for row in rows)
+              )
+          self.fail('Finish the test!')
     
     
 if __name__ == '__main__':#(6)
