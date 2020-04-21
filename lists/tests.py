a0d601dd537eaa95fbd45bc1e8b938cd3c80d9ad
hanzy429 <hanzy429@outlook.com>
2020-04-21 11:44:47 +0800

    Redirect after POST, and show all items in template

--- lists/tests.py
+++ lists/tests.py
@@ -11,10 +11,20 @@ class HomePageTest(TestCase):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
 
-    def test_can_save_a_POST_request(self):
-        response = self.client.post('/',data={'item_text':'A new list item'})
-        self.assertIn('A new list item', response.content.decode())
-        self.assertTemplateUsed(response, 'home.html')
+    ##def test_can_save_a_POST_request(self):
+    ##    self.response = self.client.post('/',data={'item_text':'A new list item'})
+    ##    self.assertIn('A new list item', response.content.decode())
+    ##    self.assertTemplateUsed(response, 'home.html')
+
+    def test_displays_all_list_item(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        response = self.client.get('/')
+
+        self.assertIn('itemey 1', response.content.decode())
+        self.assertIn('itemey 2', response.content.decode())
+
     ##def test_root_url_resolve_to_home_page_view(self):
     ##   found = resolve('/')
     ##    self.assertEqual(found.func,home_page)
@@ -44,5 +54,23 @@ class ItemModelTest(TestCase):
         first_save_item = saved_items[0]
         second_save_item = saved_items[1]
         self.assertEqual(first_save_item.text, 'The first (ever) list item')
-        ##self.assertEqual(second_save_item.text, 'Item the second')
+        self.assertEqual(second_save_item.text, 'Item the second')
+    def test_can_save_a_POST_request(self):
+        self.client.post('/', data={'item_text': 'A new list item'})
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+        ##self.assertIn('A new list item', response.content.decode())
+        ##self.assertTemplateUsed(response, 'home.html')
+    def test_redirects_after_POST(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+        
+
+    def test_only_saves_items_when_necessary(self):
+        self.client.get('/')
+        self.assertEqual(Item.objects.count(),0)
 # Create your tests here.
